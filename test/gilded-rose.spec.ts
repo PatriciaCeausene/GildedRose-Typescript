19eeb2e4edaf6e3b3771f76a2cb493e8457b9e48
PatriciaCeausene <Oana.Ceausene@softwire.com>
2023-06-28 14:46:04 +0300

    Solved GildedRose-Typescript task

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,112 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('should test usual product', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('foo', 2, 5) ]);
+        const expectedItems = [ new Item('foo', 1, 4) ];
+
+        // When
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+
+        // Then
+        expect(items).deep.equal(expectedItems);
     });
 
+    it('should test aged brie', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 2, 5) ]);
+        const expectedItems = [ new Item('Aged Brie', 1, 6) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+    it('should test aged brie', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 2, 50) ]);
+        const expectedItems = [ new Item('Aged Brie', 1, 50) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+    it('should test sufluras', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+    it('should test backstage passes for more than 10 days', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 4, 5) ]);
+        const expectedItems = [ new Item('Backstage passes to a TAFKAL80ETC concert', 3, 8) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+
+    it('should test backstage passes for less than 10 days', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 9, 5) ]);
+        const expectedItems = [ new Item('Backstage passes to a TAFKAL80ETC concert', 8, 7) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+    it('should test backstage passes for less than 5 days', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 4, 10) ]);
+        const expectedItems = [ new Item('Backstage passes to a TAFKAL80ETC concert', 3, 13) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+    it('should test backstage passes after the concert', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10) ]);
+        const expectedItems = [ new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
+
+    it('should test conjured', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Conjured Mana Cake', 10, 10) ]);
+        const expectedItems = [ new Item('Conjured Mana Cake', 9, 8) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Then
+        expect(items).deep.equal(expectedItems);
+    });
 });
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,3 +1,19 @@
 import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from "chai";
 
 // Add a master test here
+describe('testing updating products', () => {
+
+    it('should follow the rules', function() {
+        // Given
+        const gildedRose = new GildedRose([ new Item('Aged Brie', 30, 20), new Item('Backstage passes to a TAFKAL80ETC concert', 10, 30),new Item('Sulfuras, Hand of Ragnaros', 0, 30), new Item('Backstage passes to a TAFKAL80ETC concert', 4, 30), new Item('new product', 3, 0) ]);
+        const expectedItems: Item[] =  [ new Item('Aged Brie', 29, 21),new Item('Backstage passes to a TAFKAL80ETC concert', 9, 32),  new Item('Sulfuras, Hand of Ragnaros', 0, 30), new Item('Backstage passes to a TAFKAL80ETC concert', 3, 33) , new Item('new product', 2, 0) ];
+
+        // When
+        const items = gildedRose.updateQuality();
+
+        // Test
+        expect(items).deep.equal(expectedItems);
+    });
+
+});
